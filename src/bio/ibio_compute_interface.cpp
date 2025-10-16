da1144d727b734367227b8fbac0f9b18111add5c
jadaela-ara <jadaela@gmail.com>
2025-10-16 21:00:03 +0200

    Implement IBioComputeInterface class

---
+++ src/bio/ibio_compute_interface.cpp
@@ -0,0 +1,14 @@
+#include "bio/ibio_compute_interface.h"
+
+namespace BioMining {
+namespace Bio {
+
+IBioComputeInterface::IBioComputeInterface(QObject* parent)
+    : QObject(parent)
+{
+}
+
+IBioComputeInterface::~IBioComputeInterface() = default;
+
+} // namespace Bio
+} // namespace BioMining
